0a1e22a8e10dcfaaf51f0fa82db8a364f25c83ad
Victor <Vmalho393@west-mec.edu>
2023-06-30 13:41:59 -0700

    Made all functionalities except custom

--- README.md
+++ README.md
@@ -1,70 +1,58 @@
-# Getting Started with Create React App
+# MAGIC 3CRYPT
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+MAGIC 3CRYPT is a versatile encryption and decryption application that supports multiple encryption methods. With this application, you can encrypt your messages using various techniques such as Shift, Hexadecimal, Binary, Octal, and even customize your encryption order.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+- Encrypt messages using different encryption methods
+- Decrypt messages to their original form
+- Choose from a variety of encryption methods:
+  - Shift: Shifts the characters in the message by a specified hash
+  - Hexadecimal: Converts the message to a hexadecimal representation
+  - Binary: Converts the message to a binary representation
+  - Octal: Converts the message to an octal representation
+  - Custom: Allows you to choose the order of encryption methods
+- Simple and intuitive user interface
 
-### `npm start`
+## Technologies Used
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- React: JavaScript library for building user interfaces
+- HTML: Markup language for creating the structure of the application
+- CSS: Styling language for enhancing the appearance of the application
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Getting Started
 
-### `npm test`
+1. Clone the repository:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+   ```bash
+   git clone https://github.com/your-username/magic-3crypt.git
+Navigate to the project directory:
 
-### `npm run build`
+bash
+Copy code
+cd magic-3crypt
+Install the dependencies:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+bash
+Copy code
+npm install
+Start the application:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+bash
+Copy code
+npm start
+Open your web browser and access the application at http://localhost:3000.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+Usage
+Select an encryption method by clicking on the corresponding tab.
+Enter your message in the "Message" input field.
+Provide the necessary encryption parameters (e.g., hash value).
+Click the "Encrypt" button to encrypt the message.
+The encrypted message will be displayed in the respective field.
+To decrypt the message, click the "Decrypt" button.
+The decrypted message will be displayed in the respective field.
+Contributing
+Contributions are welcome! If you have any suggestions, improvements, or bug fixes, please submit a pull request. Ensure your changes adhere to the coding conventions and maintain the overall code quality.
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+License
+This project is licensed under the MIT License.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,368 @@
-import './index.css';
+import React, { useState } from 'react';
 
-const App=() => {
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [hash, setHash] = useState('');
+  const [encryptedMessage, setEncryptedMessage] = useState('');
+  const [decryptedMessage, setDecryptedMessage] = useState('');
+  const [activeTab, setActiveTab] = useState('shift');
+  const [encryptionOrder, setEncryptionOrder] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleEncrypt = (e) => {
+    e.preventDefault();
+    let result = message;
+    const selectedEncryptionOrder = encryptionOrder.length > 0 ? encryptionOrder : [activeTab];
+
+    try {
+      selectedEncryptionOrder.forEach((encryptionMethod) => {
+        switch (encryptionMethod) {
+          case 'shift':
+            result = shiftStringByHash(result, hash);
+            break;
+          case 'hexadecimal':
+            result = convertToHexadecimal(result);
+            break;
+          case 'binary':
+            result = convertToBinary(result);
+            break;
+          case 'octal':
+            result = convertToOctal(result);
+            break;
+          default:
+            throw new Error(`Invalid encryption method: ${encryptionMethod}`);
+        }
+      });
+
+      setEncryptedMessage(result);
+      setDecryptedMessage('');
+      setErrorMessage('');
+    } catch (error) {
+      setErrorMessage(error.message);
+      setEncryptedMessage('');
+      setDecryptedMessage('');
+    }
+  };
+
+  const handleDecrypt = (e) => {
+    e.preventDefault();
+    let result = encryptedMessage;
+    const selectedEncryptionOrder = encryptionOrder.length > 0 ? encryptionOrder.reverse() : [activeTab];
+
+    try {
+      selectedEncryptionOrder.forEach((encryptionMethod) => {
+        switch (encryptionMethod) {
+          case 'shift':
+            result = decryptStringByHash(result, hash);
+            break;
+          case 'hexadecimal':
+            result = convertFromHexadecimal(result);
+            break;
+          case 'binary':
+            result = convertFromBinary(result);
+            break;
+          case 'octal':
+            result = convertFromOctal(result);
+            break;
+          default:
+            throw new Error(`Invalid encryption method: ${encryptionMethod}`);
+        }
+      });
+
+      setDecryptedMessage(result);
+      setEncryptedMessage('');
+      setErrorMessage('');
+    } catch (error) {
+      setErrorMessage(error.message);
+      setDecryptedMessage('');
+      setEncryptedMessage('');
+    }
+  };
+
+  const shiftStringByHash = (inputString, hash) => {
+    if (inputString.length === 0) {
+      return '';
+    }
+
+    const normalizedHash = normalizeHash(hash);
+    let shiftedString = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      let shiftedCharCode;
+
+      if (charCode >= 97 && charCode <= 122) { // Check if the character is lowercase letter (a-z)
+        const adjustedHash = normalizedHash[i % normalizedHash.length];
+        shiftedCharCode = ((charCode - 97 + adjustedHash + 26) % 26) + 97;
+      } else {
+        shiftedCharCode = charCode; // Preserve non-alphabet characters as is
+      }
+
+      const shiftedChar = String.fromCharCode(shiftedCharCode);
+      shiftedString += shiftedChar;
+    }
+
+    return shiftedString;
+  };
+
+  const decryptStringByHash = (inputString, hash) => {
+    if (inputString.length === 0) {
+      return '';
+    }
+
+    const normalizedHash = normalizeHash(hash);
+    let decryptedString = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      let decryptedCharCode;
+
+      if (charCode >= 97 && charCode <= 122) { // Check if the character is lowercase letter (a-z)
+        const adjustedHash = normalizedHash[i % normalizedHash.length];
+        decryptedCharCode = ((charCode - 97 - adjustedHash + 26) % 26) + 97;
+      } else {
+        decryptedCharCode = charCode; // Preserve non-alphabet characters as is
+      }
+
+      const decryptedChar = String.fromCharCode(decryptedCharCode);
+      decryptedString += decryptedChar;
+    }
+
+    return decryptedString;
+  };
+
+  const normalizeHash = (hash) => {
+    const normalizedHash = [];
+
+    for (let i = 0; i < hash.length; i++) {
+      const charCode = hash.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) { // Check if the character is uppercase letter (A-Z)
+        normalizedHash.push(charCode - 65);
+      } else if (charCode >= 48 && charCode <= 57) { // Check if the character is a digit (0-9)
+        normalizedHash.push(charCode - 48);
+      } else {
+        throw new Error('Invalid hash. Only alphanumeric characters are allowed.');
+      }
+    }
+
+    return normalizedHash;
+  };
+
+  const convertToHexadecimal = (inputString) => {
+    let hexadecimalString = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      const hexadecimalChar = charCode.toString(16);
+      hexadecimalString += hexadecimalChar;
+    }
+
+    return hexadecimalString;
+  };
+
+  const convertFromHexadecimal = (hexadecimalString) => {
+    let result = '';
+
+    for (let i = 0; i < hexadecimalString.length; i += 2) {
+      const hexChar = hexadecimalString.substr(i, 2);
+      const charCode = parseInt(hexChar, 16);
+      const char = String.fromCharCode(charCode);
+      result += char;
+    }
+
+    return result;
+  };
+
+  const convertToBinary = (inputString) => {
+    let binaryString = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      const binaryChar = charCode.toString(2);
+      binaryString += binaryChar.padStart(8, '0');
+    }
+
+    return binaryString;
+  };
+
+  const convertFromBinary = (binaryString) => {
+    let result = '';
+
+    for (let i = 0; i < binaryString.length; i += 8) {
+      const binaryChar = binaryString.substr(i, 8);
+      const charCode = parseInt(binaryChar, 2);
+      const char = String.fromCharCode(charCode);
+      result += char;
+    }
+
+    return result;
+  };
+
+  const convertToOctal = (inputString) => {
+    let octalString = '';
+
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      const octalChar = charCode.toString(8);
+      octalString += octalChar;
+    }
+
+    return octalString;
+  };
+
+  const convertFromOctal = (octalString) => {
+    let result = '';
+
+    for (let i = 0; i < octalString.length; i += 3) {
+      const octalChar = octalString.substr(i, 3);
+      const charCode = parseInt(octalChar, 8);
+      const char = String.fromCharCode(charCode);
+      result += char;
+    }
+
+    return result;
+  };
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+    setEncryptionOrder([]);
+    setErrorMessage('');
+    setEncryptedMessage('');
+    setDecryptedMessage('');
+  };
+
+  const handleEncryptionOrderChange = (e) => {
+    const selectedEncryptionOrder = Array.from(e.target.selectedOptions, (option) => option.value);
+    setEncryptionOrder(selectedEncryptionOrder);
+    setErrorMessage('');
+    setEncryptedMessage('');
+    setDecryptedMessage('');
+  };
+
   return (
-    <div>
-      <form>
-        <div className='flex flex-col'>
-          <label htmlFor='hash'>Hash #: </label> <input type="number" name="hash" id="hash" />
-          <label htmlFor='message'>Message: </label> <textarea name='message' id='message'></textarea>
+    <div className=' flex justify-center h-screen m-auto w-[32rem]'>
+    <div className="flex justify-center h-screen w-[32rem]  border-2 border-black rounded-3xl ">
+      <div className=" rounded-lg  p-6 ">
+        <h1 className="text-3xl font-bold mb-4 text-center">MAGIC 3CRYPT</h1>
+        <div className="mb-4">
+          <ul className="flex justify-center space-x-4">
+            <li
+              className={`cursor-pointer p-2 ${activeTab === 'shift' ? 'text-blue-500 underline bg-slate-700 rounded-md' : 'bg-slate-300 text-gray-500 rounded-md'}`}
+              onClick={() => handleTabChange('shift')}
+            >
+              Shift
+            </li>
+            <li
+              className={`cursor-pointer p-2 ${activeTab === 'hexadecimal' ? 'text-blue-500 underline bg-slate-700 rounded-md' : 'bg-slate-300 text-gray-900 rounded-md'}`}
+              onClick={() => handleTabChange('hexadecimal')}
+            >
+              Hexadecimal
+            </li>
+            <li
+              className={`cursor-pointer p-2 ${activeTab === 'binary' ? 'text-blue-500 underline bg-slate-700 rounded-md' : 'bg-slate-300 text-gray-900 rounded-md'}`}
+              onClick={() => handleTabChange('binary')}
+            >
+              Binary
+            </li>
+            <li
+              className={`cursor-pointer p-2 ${activeTab === 'octal' ? 'text-blue-500 underline bg-slate-700 rounded-md' : 'bg-slate-300 text-gray-900 rounded-md'}`}
+              onClick={() => handleTabChange('octal')}
+            >
+              Octal
+            </li>
+            <li
+              className={`cursor-pointer p-2 ${activeTab === 'custom' ? 'text-blue-500 underline bg-slate-700 rounded-md' : 'bg-slate-300 text-gray-900 rounded-md'}`}
+              onClick={() => handleTabChange('custom')}
+            >
+              Custom
+            </li>
+          </ul>
         </div>
-        <button>Encrypt</button> <button>Decrypt</button>
-      </form>
-      <div className='' id='result'></div>
-    </div>
+        {activeTab === 'custom' && (
+          <div className="mb-4">
+            <label htmlFor="encryptionOrder" className="block mb-1 font-medium">Encryption Order:</label>
+            <select
+              id="encryptionOrder"
+              multiple
+              value={encryptionOrder}
+              onChange={handleEncryptionOrderChange}
+              className="w-full p-2 border rounded-md"
+            >
+              <option value="shift">Shift</option>
+              <option value="hexadecimal">Hexadecimal</option>
+              <option value="binary">Binary</option>
+              <option value="octal">Octal</option>
+            </select>
+          </div>
+        )}
+        <form>
+          <div className="mb-4">
+            <label htmlFor="message" className="block mb-1 font-medium">Message:</label>
+            <input
+              type="text"
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full p-2 border border-black rounded-md"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="hash" className="block mb-1 font-medium">Hash:</label>
+            <input
+              type="text"
+              id="hash"
+              value={hash}
+              onChange={(e) => setHash(e.target.value)}
+              className="w-full p-2 border border-black rounded-md"
+            />
+          </div>
+          <div className="mb-4 flex flex-row items-center justify-center">
+            <button
+              onClick={handleEncrypt}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md mr-10"
+            >
+              Encrypt
+            </button>
+            <button
+              onClick={handleDecrypt}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md"
+            >
+              Decrypt
+            </button>
+          </div>
+        </form>
+        {encryptedMessage && (
+          <div className="mb-4">
+            <label htmlFor="encryptedMessage" className="block mb-1 font-medium">Encrypted Message:</label>
+            <textarea
+              id="encryptedMessage"
+              value={encryptedMessage}
+              readOnly
+              className="w-full p-2 border border-black rounded-md"
+            />
+          </div>
+        )}
+        {decryptedMessage && (
+          <div className="mb-4">
+            <label htmlFor="decryptedMessage" className="block mb-1 font-medium">Decrypted Message:</label>
+            <textarea
+              id="decryptedMessage"
+              value={decryptedMessage}
+              readOnly
+              className="w-full p-2 border border-black rounded-md"
+            />
+          </div>
+        )}
+        {errorMessage && (
+          <div className="mb-4 text-red-500">
+            <p>{errorMessage}</p>
+          </div>
+        )}
+      </div>
+      </div>
+      </div>
   );
-}
+};
 
 export default App;
